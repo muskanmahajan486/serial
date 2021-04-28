c5cd66a6308c7a6f8283d2fde3f62cd09f8b557e
Danu andrean <58714116+danmirror@users.noreply.github.com>
2021-04-28 16:51:16 +0700

    Update rec.ino

--- micro_to_micro/rec/rec.ino
+++ micro_to_micro/rec/rec.ino
@@ -7,6 +7,9 @@
 #include <SoftwareSerial.h>
 SoftwareSerial ser(D6,D5);
 
+int starts = 0;
+String len = "";
+
 void setup() {
   ser.begin(9600);
   Serial.begin(9600);
@@ -16,10 +19,32 @@ void loop() {
   String content = "";
   if (ser.available()) {
     while (ser.available()) {
-      content += (char)ser.read();
+      
+      char data = (char)ser.read() ;
+//      Serial.print(starts);/
+      
+      if(data=='*')
+        starts =1;
+        
+      else if(data =='#'){
+          starts =0;
+          break;
+        }
+        
+      else if(starts == 1){
+        if(input == 0)
+          len + = data;
+        
+        if(data == '/')
+          input =1;
+        
+        else if(input == 1)
+          content += data;
+      }
+      
     }
   }
   Serial.println(content);
-  delay(1000);
+  delay(3000);
   content = "";
 }
